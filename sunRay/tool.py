e5ce9e7d02d75d46e5fe82edeee9112d29e49825
Peijin <pjer1316@icloud.com>
2020-07-12 13:28:42 +0800

    tool finish

--- sunRay/tool.py
+++ sunRay/tool.py
@@ -1,11 +1,27 @@
 # tool set
+# f : freq [Hz]
+# h : height [R_s]
 
 import sunRay.plasmaFreq as pf 
 import sunRay.densityModel as dm  
+from scipy.optimize import fsolve 
+import torch
+import numpy as np
+PI = torch.acos(torch.Tensor([-1]))
 
+def R_to_freq(R,f_ratio,ne_r = dm.parkerfit):
+    """
+    Wave frequency from R
+    """
+    return pf.omega_pe_r_np(ne_r,torch.Tensor([R])) /2/PI  *f_ratio
 
-def height_to_freq(h,f_ratio,dens = dm.parkerfit):
-    pass
+def freq_to_R(f,f_ratio,ne_r = dm.parkerfit):
+    """
+    Starting height for wave frequency f
+    """
+    f_pe = f/f_ratio
+    func  = lambda R : f_pe - (pf.omega_pe_r_np(ne_r,torch.Tensor([R])) /2/PI).numpy()[0]
 
-def freq_to_height(f,f_ratio,dens = dm.parkerfit):
-    pass
+    R_solution = fsolve(func, 2) # solve the R
+
+    return R_solution # [R_s]
